20d8d54b31d2df9320768f433d7bbc2e1dbf71da
Tordek <tordektopor@gmail.com>
2018-10-10 14:23:39 +0300

    Added main to TestGit and compiled and run the program.

--- car_task/src/car_task/filter/TestGit.java
+++ car_task/src/car_task/filter/TestGit.java
@@ -4,4 +4,9 @@ public class TestGit {
 	public String testMethod() {
 		return "Hello";
 	}
+	
+	public static void main(String[] args) {
+		TestGit tg = new TestGit();
+		System.out.println(tg.testMethod());
+	}
 }
